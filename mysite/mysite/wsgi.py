336580b66e2437ae9d3cb80a24b751a3518c9c91
ivan_ilin <ivaniliin@icloud.com>
2024-07-19 22:00:23 +0200

    wsgi file updated

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
